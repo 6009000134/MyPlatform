2d9bed989f2cbcb8efa4c5152255cf9482bca978
6009000134 <491675469@qq.com>
2021-09-26 18:26:35 +0800

    ss

--- MyPlatform/Model/Query/QueryView.cs
+++ MyPlatform/Model/Query/QueryView.cs
@@ -12,10 +12,103 @@ namespace MyPlatform.Model.Query
     public class QueryView
     {
         /// <summary>
-        /// 查询页面名称
+        /// ID
+        /// </summary>
+        public int ID { get; set; }
+        /// <summary>
+        /// 创建人
+        /// </summary>
+        public string CreatedBy { get; set; }
+        /// <summary>
+        /// 创建日期
+        /// </summary>
+        public DateTime CreatedDate { get; set; }
+        /// <summary>
+        /// 创建日期
+        /// </summary>
+        public string UpdatedBy { get; set; }
+        /// <summary>
+        /// 创建日期
+        /// </summary>
+        public DateTime UpdatedDate { get; set; }
+        /// <summary>
+        /// 简称
+        /// </summary>
+        public string ShortName { get; set; }
+        /// <summary>
+        /// 显示名称
+        /// </summary>
+        public string DisplayName { get; set; }
+        /// <summary>
+        /// 类型（表/视图/存储过程）
+        /// </summary>
+        public string Type { get; set; }
+        /// <summary>
+        /// 名称（表名/视图名/存储过程名）
         /// </summary>
         public string Name { get; set; }
-
-        
     }
+    public class QueryViewDetail
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int ID { get; set; }
+        /// <summary>
+        /// 创建人
+        /// </summary>
+        public string CreatedBy { get; set; }
+        /// <summary>
+        /// 创建日期
+        /// </summary>
+        public DateTime CreatedDate { get; set; }
+        /// <summary>
+        /// 创建日期
+        /// </summary>
+        public string UpdatedBy { get; set; }
+        /// <summary>
+        /// 创建日期
+        /// </summary>
+        public DateTime UpdatedDate { get; set; }
+        /// <summary>
+        /// ViewID
+        /// </summary>
+        public string ViewID { get; set; }
+        /// <summary>
+        /// 列名
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 显示名称
+        /// </summary>
+        public string DisplayName { get; set; }
+        /// <summary>
+        /// 数据库类型
+        /// </summary>
+        public string Type { get; set; }
+    }
+    public class SqlServerDBType
+    {
+        /// <summary>
+        /// ColID
+        /// </summary>
+        public string ID { get; set; }
+        /// <summary>
+        /// ColID
+        /// </summary>
+        public string ColID { get; set; }
+        /// <summary>
+        /// 列名
+        /// </summary>
+        public string Type { get; set; }
+        /// <summary>
+        /// 显示名称
+        /// </summary>
+        public int Size { get; set; }
+        /// <summary>
+        /// 数据库类型
+        /// </summary>
+        public int Precision { get; set; }
+    }
+
 }
